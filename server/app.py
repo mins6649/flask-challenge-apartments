6a052e0f5bf4e4ce4ebcc898241bf36341e4a027
minyoungshin <mins6649@gmail.com>
2023-04-12 11:01:38 -0400

    first attempt

--- server/app.py
+++ server/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, request #add request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
-from models import db
+# add (Apartment, Tenant, Lease)
+from models import db, Apartment, Tenant, Lease
 
 app = Flask( __name__ )
 app.config[ 'SQLALCHEMY_DATABASE_URI' ] = 'sqlite:///apartments.db'
@@ -10,7 +11,86 @@ app.config[ 'SQLALCHEMY_TRACK_MODIFICATIONS' ] = False
 
 migrate = Migrate( app, db )
 db.init_app( app )
+# added API
+api = Api(app)
 
+class Apartment(Resource):
+    def get(self):
+        
+        apartment_dict_list = [a.to_dict() for a in Apartment.query.all()]
+
+        response = make_response(
+            apartment_dict_list,
+            200
+        ) 
+        return response
+    
+    def post(self):
+        
+        new_apartment = Apartment(
+            number = request.form['number'],
+        )
+
+        db.session.add(new_apartment)
+        db.session.commit()
+
+        response_dict = new_apartment.to_dict()
+
+        response = make_response(
+            response_dict,
+            201,
+        )
+
+        return response
+
+
+api.add_resource(Apartment, '/apartment')
+
+class ApartmentById(Resource):
+    def get(self, id):
+        
+        response_dict = Apartment.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            response_dict,
+            200
+        )
+        return response
+    
+    def patch(self, id):
+        
+        record = Apartment.query.filter_by(id=id).first()
+        for attr in request.form:
+            setattr(record, attr, request.form[attr])
+
+        db.session.add(record)
+        db.session.commit()
+
+        response_dict = record.to_dict()
+
+        response = make_response(
+            response_dict,
+            200
+        )
+        return response
+
+    def delete(self, id):
+        
+        record = Apartment.query.filter_by(id=id).first()
+
+        db.session.delete(record)
+        db.session.commit()
+
+        response_dict = {"message": "record successfully deleted"}
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+
+api.add_resource(Apartment, '/apartment/<int:id>')
 
 if __name__ == '__main__':
     app.run( port = 3000, debug = True )
--- server/models.py
+++ server/models.py
@@ -5,3 +5,42 @@ from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 
 db = SQLAlchemy()
+
+class Apartment(db.Model, SerializerMixin):
+    __tablename__ = 'apartments'
+     
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer)
+     
+    leases = db.relationship('Lease', backref='apartment')
+
+    # 
+    serialize_rules = ('-leases.apartment')
+
+class Tenant(db.Model, SerializerMixin):
+    __tablename__ = 'tenants'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer) #Must be over 18
+
+    leases = db.relationship('Lease', backref='tenants')
+
+    # 
+    serialize_rules = ('-leases.tenant')
+
+class Lease(db.Model, SerializerMixin):
+    __tablename__ = 'leases'
+
+    id = db.Column(db.Integer, primary_key = True)
+    rent = db.Column(db.Integer)
+
+    apartment_id = db.Column(db.Integer, db.ForeignKey('apartments.id'))
+    tenant_id = db.Column(db.Integer, db.ForeignKey('tenants.id'))
+
+    # 
+    serialize_rules = ('-apartment.leases', '-tenant.leases')
+    # when do we use serialization 
+
+
+# HOW TO SEED :-(
